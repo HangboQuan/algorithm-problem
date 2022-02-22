a93c0debb68eea044782294403a5f9b40617d780
quanhangbo <2409711401@qq.com>
2022-02-22 20:57:42 +0800

    单调栈&&最大公约数

--- acwing_base/week2/acwing830.cpp
+++ acwing_base/week2/acwing830.cpp
@@ -6,7 +6,29 @@ using namespace std;
 
 const int N = 100010;
 int stk[N], tt;
+//欧几里得算法(辗转相除法)
+/**
+ * 即 证明gcd(a, b) = gcd(b, a % b)
+ *
+ * 证明：设c为a, b的公因子,则a = mc, b = nc, r = a % b存在k使得 r = a - kb, 则r = mc - knc = (m - kn)c
+ * gcd(b, a % b) = gcd(nc, (m - kn)c) = gcd(n, (m - kn))c
+ * gcd(a, b) = gcd(mc, nc) = gcd(m, n)c(c != 0)
+ * 则gcd(m, n) = gcd(n, m - kn) => gcd(m, n) = gcd(n, m % n)
+ */
+int gcd(int a, int b){
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int gcd2(int a, int b){
+    while(b != 0){
+        int c = a;
+        a = b;
+        b = c % b;
+    }
+    return a;
+}
 int main(){
+
     int n;
     scanf("%d", &n);
 
@@ -19,5 +41,11 @@ int main(){
 
         stk[++ tt] = x;
     }
+
+
+    // 给定任意的两个数求最大公约数
+    int a = 32, b = 20;
+    int res = gcd2(a, b);
+    cout << res << endl;
     return 0;
 }
