189c562ecbfed7f238419923a55e773e5d912c0a
quanhangbo <2409711401@qq.com>
2023-01-05 22:33:10 +0800

    stl pair and vector

--- acwing_base/study/STL/pair.cpp
+++ acwing_base/study/STL/pair.cpp
@@ -4,7 +4,20 @@
 
 #include<iostream>
 #include<utility>
+#include<bits/stdc++.h>
 using namespace std;
+
+/**
+ * C++ Standard Template Library
+ * it is a set of C++ template class to provide common programming data structures and functions such as
+ * lists, stacks, arrays, etc.
+ *
+ * STL has 4 components:
+ * 1.Algorithms
+ * 2.Containers
+ * 3.Functions
+ * 4.Iterators
+ */
 
 /**
  * pair is used to combine together two values that may be of different data types. Pair provides a way to store
@@ -30,7 +43,8 @@ int main() {
     // 0 0
     cout << PAIR2.first << " " << PAIR2.second << endl;
 
-    // another valid syntax to declare pair or initialize a pair
+    // another valid syntax to declare pair or initialize a pair, This template function allows to
+    // create a value pair without writing the types explicitly.
     pair<int, char> PAIR3 = {1, 'a'};
     pair<int, char> PAIR4 = make_pair(2, 'b');
     pair<string, double> PAIR5("Geeks", 1.23);
@@ -40,5 +54,35 @@ int main() {
     cout << PAIR3.first << " " << PAIR3.second << endl;
     cout << PAIR4.first << " " << PAIR4.second << endl;
     cout << PAIR5.first << " " << PAIR5.second << endl;
+
+    // 交换两个pair对象，前提是pair的类型 This function swaps the contents of one pair object with the contents of
+    // another pair object. The pairs must be of the same type.
+    PAIR3.swap(PAIR4);
+    cout << PAIR3.first << " " << PAIR3.second << endl;
+
+
+    // tie() tie(int &, int &) = pair1
+    // This function works the same as in tuples. It creates a tuple of lvalue references to its arguments,
+    // to unpack the tuple values into separate variables. 类似于元组的数据结构，创建了一个左值引用的元组，把元组的值
+    // 压缩为单个值? tie()的实际用处是什么?
+    pair<int, int> pair1 = {1, 2};
+    int a, b;
+    tie(a, b) = pair1;
+    // 引用赋值 1 2
+    cout << a << " " << b << endl;
+
+    pair<int, int> pair2 = {3, 4};
+    // 3 2
+    tie(a, ignore) = pair2;
+    cout << a << " " << b << endl;
+
+    pair<int, pair<int, char>> pair3 = {3, {4, 'a'}};
+    int x, y;
+    char z;
+    x = pair3.first;
+    // tie() => 通过引用来进行赋值
+    tie(y, z) = pair3.second;
+    // 3 4 a
+    cout << x << " " << y << " " << z << endl;
     return 0;
 }
---
+++ acwing_base/study/STL/vector.cpp
@@ -0,0 +1,11 @@
+//
+// Created by quanhangbo on 2023/1/5 22:22.
+//
+#include<iostream>
+#include<vector>
+
+// Vectors are the sames as dynamic arrays with the ability to resize itself automatically when an element is inserted
+// or deleted, with their storage being handled automatically by the container
+int main() {
+
+}
