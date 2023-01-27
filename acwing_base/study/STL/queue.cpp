c284458779c9d9af5954403e973446b3cab03ecc
quanhangbo <2409711401@qq.com>
2023-01-27 23:02:45 +0800

    queue

--- acwing_base/study/STL/queue.cpp
+++ acwing_base/study/STL/queue.cpp
@@ -9,16 +9,23 @@ using namespace std;
 void showq(queue<int> gq) {
     queue<int> g = gq;
     while(!g.empty()) {
-        cout << '\n' << g.front();
+        cout << g.front() << '\n';
+        // deletes the first element of the queue
         g.pop();
     }
     cout << '\n';
 }
 int main(void) {
     queue<int> gquiz;
+    // adds the element at the end of the queue
     gquiz.push(10);
     gquiz.push(20);
     gquiz.push(30);
 
     showq(gquiz);
+
+    cout << gquiz.size() << endl;
+    // returns a reference to the first element of the queue
+    cout << gquiz.front() << endl;
+    cout << gquiz.back() << endl;
 }
