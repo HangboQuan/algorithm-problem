cfc79ba7e3052e6d5bd00b86d39d35326506633a
quanhangbo <2409711401@qq.com>
2023-02-08 22:34:35 +0800

    stack

--- acwing_base/study/STL/stack.cpp
+++ acwing_base/study/STL/stack.cpp
@@ -7,5 +7,19 @@
 using namespace std;
 
 int main(void) {
-    
+    /**
+     * Stacks are a type of container adaptors with LIFO(Last In First Out) type of working, where a new
+     * element is added at one end(top) and an element removed from that end only.
+     */
+
+    stack<int> stack;
+    stack.push(21);
+    stack.push(22);
+    stack.push(23);
+    stack.push(25);
+
+    while(!stack.empty()) {
+        cout << stack.top() << " ";
+        stack.pop();
+    }
 }
