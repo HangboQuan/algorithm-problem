2f52b969e8119bf5c75d06d64d4be451311d28b3
quanhangbo <2409711401@qq.com>
2023-02-11 21:53:31 +0800

    map

--- acwing_base/study/STL/map.cpp
+++ acwing_base/study/STL/map.cpp
@@ -3,8 +3,22 @@
 //
 #include<iostream>
 #include<map>
+#include<algorithm>
+#include<bits/stdc++.h>
 /**
  * Maps are associative containers that store elements in a mapped fashion. Each element has a key value and a
  * mapped value. No two mapped values can have the same key vales
  */
 using namespace std;
+int main(void) {
+    map<string, int> m;
+    m["one"] = 1;
+    m["two"] = 2;
+    m["three"] = 3;
+
+    map<string, int> :: iterator it = m.begin();
+    while (it != m.end()) {
+        cout << "key: " << it -> first << ", value: " << it -> second << endl;
+        ++ it;
+    }
+}
