2e109d588274afb8fb548ccd8f7ea45789b56a46
quanhangbo <2409711401@qq.com>
2023-02-27 22:31:45 +0800

    preSum + unordered_map

--- acwing_base/week1/leetcode560.cpp
+++ acwing_base/week1/leetcode560.cpp
@@ -3,6 +3,7 @@
 //
 
 #include<iostream>
+#include<unordered_map>
 #include<vector>
 using namespace std;
 
@@ -22,4 +23,19 @@ int subarraySum(vector<int>& nums, int k) {
         }
     }
     return ans;
+}
+
+int subarraySum2(vector<int>& nums, int k) {
+    unordered_map<int, int> mp;
+    mp[0] = 1;
+    int count = 0, pre = 0;
+    for(auto& x : nums) {
+        pre += x;
+        if (mp.find(pre - k) != mp.end()) {
+            count += mp[pre - k];
+        }
+        mp[pre] ++;
+    }
+    return count;
+
 }
