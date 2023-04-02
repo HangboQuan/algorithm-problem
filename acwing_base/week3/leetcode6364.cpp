998ac76726a9aed0697c8717818852faf9a6c0d7
quanhangbo <2409711401@qq.com>
2023-04-02 21:42:18 +0800

    贪心

--- acwing_base/week3/leetcode6364.cpp
+++ acwing_base/week3/leetcode6364.cpp
@@ -13,36 +13,18 @@ int arr[N];
 class Solution {
 public:
     int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
-        vector<int> ans;
-        for (int i = 0; i < reward1.size(); i ++ ) {
-            ans.push_back(reward1[i] > reward2[i] ? reward1[i] : reward2[i]);
-        }
-        sort(ans.rbegin(), ans.rend());
-
-        arr[0] = ans[0];
-        for (int i = 1; i < ans.size(); i ++ ) {
-            arr[i] = arr[i - 1] + ans[i];
+        int n = reward1.size(), ans = 0;
+        vector<int> diff(n);
+        for (int i = 0; i < n; i ++ ) {
+            diff[i] = reward1[i] - reward2[i];
+            ans += reward2[i];
         }
 
-        if (k % 2 == 1) {
-            // even
-            return arr[k - 1];
-        } else {
-            if (k * 2 <= reward1.size()) {
-                return max(arr[k - 1], arr[k * 2 - 1]);
-            }
-            return arr[k - 1];
+        sort(diff.begin(), diff.end(), greater<int>());
+        for (int i = 0; i < k; ++ i) {
+            ans += reward1[i];
         }
-
-        for (int i = 0; i < N; i ++ ) {
-            cout << arr[i] << " ";
-        }
-        // 前缀和
-        cout << endl;
-
-
-        // if k is even, return top k
-
+        return ans;
     }
 };
 
