359ba5ff917c27339dd1f928e59b72b53a1bd142
quanhangbo <2409711401@qq.com>
2023-04-03 18:41:41 +0800

    leetcode周赛

--- acwing_base/week3/leetcode6362.cpp
+++ acwing_base/week3/leetcode6362.cpp
@@ -4,49 +4,57 @@
 
 #include<string>
 #include<iostream>
+#include<bits/regex.h>
 using namespace std;
 
 
+//class Solution {
+//public:
+//    int max = 0;
+//    bool isBlanceString(string s) {
+//        if (s.size() % 2 != 0) return false;
+//        for (int i = 0; i < s.size() / 2; i ++ ) {
+//            if (s.at(i) != '0') {
+//                return false;
+//            }
+//        }
+//
+//        for (int i = s.size() / 2; i < s.size(); i ++ ) {
+//            if (s.at(i) != '1') {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    int findTheLongestBalancedSubstring(string s) {
+//
+//        if (s.size() == 0) {
+//            return 0;
+//        }
+//
+//        for (int i = 0; i < s.length(); i ++ ) {
+//            for (int j = 2; j <= s.length(); j ++ ) {
+//                if (isBlanceString(s.substr(i, j))) {
+//                    int len = s.substr(i, j).size();
+//                    if (len > max) {
+//                        max = len;
+//                    }
+//                }
+//            }
+//        }
+//        return max;
+//    }
+//};
+
 class Solution {
-
 public:
-
-    int max = 0;
-    bool isBlanceString(string s) {
-        if (s.size() % 2 != 0) return false;
-        for (int i = 0; i < s.size() / 2; i ++ ) {
-            if (s.at(i) != '0') {
-                return false;
-            }
-        }
-
-        for (int i = s.size() / 2; i < s.size(); i ++ ) {
-            if (s.at(i) != '1') {
-                return false;
-            }
-        }
-        return true;
-    }
-
+    // regex = '.*0{i}1{i}.*'
     int findTheLongestBalancedSubstring(string s) {
-
-        if (s.size() == 0) {
-            return 0;
-        }
-
-        for (int i = 0; i < s.length(); i ++ ) {
-            for (int j = 2; j <= s.length(); j ++ ) {
-                if (isBlanceString(s.substr(i, j))) {
-                    int len = s.substr(i, j).size();
-                    if (len > max) {
-                        max = len;
-                    }
-                }
-            }
-        }
-        return max;
+        
     }
 };
+
 int main(void) {
     Solution* sol = new Solution();
     cout << sol->findTheLongestBalancedSubstring("00010");
--- acwing_base/week3/leetcode6363.cpp
+++ acwing_base/week3/leetcode6363.cpp
@@ -3,47 +3,98 @@
 //
 
 #include<iostream>
+#include<algorithm>
 #include<vector>
 #include<unordered_map>
 using namespace std;
 const int N = 210;
-
 int arr[N];
+//class Solution {
+//public:
+//    vector<vector<int>> findMatrix(vector<int>& nums) {
+//        for (auto x : nums) {
+//            arr[x] ++;
+//        }
+//        vector<vector<int>> result;
+//        bool con = false;
+//        int n = 0;
+//        while(!con) {
+//            vector<int> ans;
+//            for (int i = 0; i < N; i ++ ) {
+//                if (arr[i] != 0) {
+//                    ans.push_back(i);
+//                    arr[i] -= 1;
+//                }
+//            }
+//            result.push_back(ans);
+//            int i = 0;
+//            for (; i < N; i ++ ) {
+//                if (arr[i] != 0) {
+//                    con = false;
+//                    break;
+//                }
+//            }
+//            if (i == N) con = true;
+//        }
+//        return result;
+//    }
+//};
+
+//class Solution {
+//public:
+//    vector<vector<int>> findMatrix(vector<int>& nums) {
+//        int n = nums.size(), m, i, j;
+//        vector<int> s(n + 1);
+//        for (auto& x : nums) {
+//            s[x] ++;
+//        }
+//        // max_element() 返回的是指向最大元素的迭代器
+//        // *max_element() 返回的是最大值的具体数值
+//        m = *max_element(s.begin(), s.end());
+//        cout << m << endl;
+//        vector<vector<int>> ans(m);
+//        for (i = 0; i < m; i ++ ) {
+//            for (int j = 0; j <= n; j ++) {
+//                if (s[j]) {
+//                    ans[i].push_back(j);
+//                    s[j] --;
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+//};
 
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        for (auto x : nums) {
-            arr[x] ++;
+        unordered_map<int, int> ctr;
+        vector<vector<int>> ans;
+        for(int x : nums) {
+            ++ ctr[x];
         }
-        vector<vector<int>> result;
-        bool con = false;
-        int n = 0;
-        while(!con) {
-            vector<int> ans;
 
-            for (int i = 0; i < N; i ++ ) {
-                if (arr[i] != 0) {
-                    ans.push_back(i);
-                    arr[i] -= 1;
-                }
+        for (auto& it : ctr) {
+            // 不能没有这段 resize的代码
+            if (ans.size() < it.second) {
+                ans.resize(it.second);
             }
-            result.push_back(ans);
-            int i = 0;
-            for (; i < N; i ++ ) {
-                if (arr[i] != 0) {
-                    con = false;
-                    break;
-                }
+            for (int i = 0; i < it.second; i ++ ) {
+                ans[i].push_back(it.first);
             }
-            if (i == N) con = true;
         }
-        return result;
+        return ans;
     }
 };
 
 int main(void) {
     Solution* s = new Solution;
     vector<int> nums = {1,3,4,1,2,3,1};
-    s -> findMatrix(nums);
+    vector<vector<int>> ans = (s -> findMatrix(nums));
+    for(auto& x : ans) {
+        for(auto& y : x) {
+            cout << y << " ";
+        }
+        cout << endl;
+    }
 }
