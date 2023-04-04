4b092423bbed640bd39218cbd187569f5b538f1e
quanhangbo <2409711401@qq.com>
2023-04-04 17:06:15 +0800

    dfs

--- acwing_base/week3/acwing92.cpp
+++ acwing_base/week3/acwing92.cpp
@@ -2,36 +2,50 @@
 // Created by quanhangbo on 2023/3/30 17:28.
 //
 
+// https://www.acwing.com/problem/content/94/ 递归实现指数型枚举
 #include<iostream>
 using namespace std;
 const int N = 20;
 int n;
 bool vis[N];
 int path[N];
-
-void dfs(int step, int bit) {
-    if (step == bit){
-        for (int i = 0; i < bit; i ++ ) {
-            if (path[i] != 0) {
-                printf("%d ", path[i]);
-            }
+/**
+ * result:
+1
+2
+3
+1 2
+1 3
+2 3
+1 2 3
+ * @param step
+ * @param len
+ */
+void dfs(int step, int len) {
+    if (step == len) {
+        for (int i = 0; i < len; i ++ ) {
+            printf("%d ", path[i]);
         }
         puts("");
-        return ;
     }
+
     for (int i = 1; i <= n; i ++ ) {
-        if(!vis[i] && step < bit) {
+        if (!vis[i] && (step == 0 || path[step - 1] < i)) {
             path[step] = i;
             vis[i] = true;
-            dfs(step + 1, bit + 1);
+            dfs(step + 1, len);
             vis[i] = false;
             path[step] = 0;
         }
     }
 }
 
+
 int main(void) {
+    printf("\n");
     scanf("%d", &n);
-    dfs(0, 1);
+    for (int i = 1; i <= n; i ++ ) {
+        dfs(0, i);
+    }
     return 0;
 }
