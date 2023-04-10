7be7476015cb3c5d4e2d451d46ab3d5e865b325c
quanhangbo <2409711401@qq.com>
2023-04-10 11:20:35 +0800

    acwing第98周周赛

--- acwing_base/week3/acwing4948.cpp
+++ acwing_base/week3/acwing4948.cpp
@@ -2,52 +2,86 @@
 // Created by quanhangbo on 2023/4/9 17:22.
 //
 
+//#include<iostream>
+//using namespace std;
+//
+//const int N = 100010;
+//string arr[N];
+//int n;
+//
+//int main() {
+//
+//    scanf("%d", &n);
+//    string result;
+//    int countZero = 0;
+//    for (int i = 0; i < n; i ++ ) {
+//        cin >> arr[i];
+//        if (arr[i] == "0") {
+//            cout << 0 << endl;
+//            return 0;
+//        }
+//        // 判断是否为美丽数
+//        if (arr[i][0] == '1') {
+//            int left = arr[i].length() - 1 > 0 ? arr[i].length() - 1 : 0;
+//            if (left > 0) {
+//                int j = 1;
+//                for (; j < arr[i].length(); j ++ ) {
+//                    if (arr[i][j] != '0') {
+//                        break;
+//                    }
+//                }
+//                if (j == arr[i].length()) {
+//                    countZero += j - 1;
+//                } else {
+//                    result = arr[i];
+//                }
+//
+//            }
+//        } else {
+//            result = arr[i];
+//        }
+//    }
+//    if (result == "") result = "1";
+//    cout << result;
+//    for (int i = 0; i < countZero; i ++ ) {
+//        cout << 0;
+//    }
+//    return 0;
+//}
+
 #include<iostream>
 using namespace std;
 
-const int N = 100010;
-string arr[N];
 int n;
+const int N = 100010;
 
-int main() {
-
+string str[N];
+int main(void) {
     scanf("%d", &n);
-    string result;
-    int countZero = 0;
+    int k = 0;
     for (int i = 0; i < n; i ++ ) {
-        cin >> arr[i];
-        if (arr[i] == "0") {
+        string s;
+        cin >> s;
+        str[i] = s;
+        if (s == "0") {
             cout << 0 << endl;
             return 0;
         }
-        // 判断是否为美丽数
-        if (arr[i][0] == '1') {
-            int left = arr[i].length() - 1 > 0 ? arr[i].length() - 1 : 0;
-            if (left > 0) {
-                int j = 1;
-                for (; j < arr[i].length(); j ++ ) {
-                    if (arr[i][j] != '0') {
-                        break;
-                    }
-                }
-                if (j == arr[i].length()) {
-                    countZero += j - 1;
-                } else {
-                    result = arr[i];
-                }
+        int cnt = 0;
+        for (auto c : s) {
+            if (c > '1') k = i;
+            else if (c == '1') cnt ++;
+        }
 
-            }
-        } else {
-            result = arr[i];
+        if (cnt > 1) {
+            k = i;
         }
     }
-    if (result == "") result = "1";
-    cout << result;
-    for (int i = 0; i < countZero; i ++ ) {
-        cout << 0;
+    cout << str[k];
+    for (int i = 0; i < n; i ++ ) {
+        if (i != k) {
+            cout << str[i].substr(1);
+        }
     }
     return 0;
-
-
-
 }
--- acwing_base/week3/acwing4949.cpp
+++ acwing_base/week3/acwing4949.cpp
@@ -2,58 +2,94 @@
 // Created by quanhangbo on 2023/4/9 18:45.
 //
 
+//#include<iostream>
+//#include<vector>
+//#include<unordered_map>
+//using namespace std;
+//
+//const int N = 1000010;
+//
+//int m;
+//int countTwo = 0;
+//int countFive = 0;
+//int countTen = 0;
+//
+//int main() {
+//    scanf("%d", &m);
+//    unordered_map<int, int> mt;
+//    for (int i = 1; i <= N; i ++ ) {
+//        int rt = i;
+//        while (rt % 10 == 0 && rt != 0) {
+//            countTen ++;
+//            rt /= 10;
+//        }
+//        while (rt % 5 == 0 && rt != 0) {
+//            countFive ++;
+//            rt /= 5;
+//        }
+//        while (rt % 2 == 0 && rt != 0) {
+//            countTwo ++;
+//            rt /= 2;
+//        }
+//
+//        mt[i] = min(countTwo, countFive) + countTen;
+//    }
+//
+//    int i = 1;
+//    bool tag = false;
+//    vector<int> vec;
+//    int count = 0;
+//    for (; i <= N; i ++ ) {
+//        if (mt[i] == m) {
+//            count ++;
+//            vec.push_back(i);
+//            tag = true;
+//        }
+//
+//        if (mt[i] > m && !tag) {
+//            cout << 0 << endl;
+//            return 0;
+//        }
+//    }
+//    cout << count << endl;
+//    for (auto& x : vec) {
+//        cout << x << " ";
+//    }
+//    return 0;
+//}
+
 #include<iostream>
 #include<vector>
-#include<unordered_map>
 using namespace std;
 
-const int N = 1000010;
-
 int m;
-int countTwo = 0;
-int countFive = 0;
-int countTen = 0;
 
-int main() {
+// 这段代码的时间复杂度 为什么是O(nlogn)
+int f(int n, int p) {
+    int res = 0;
+    while(n) {
+        res += n / p;
+        n /= p;
+    }
+    return res;
+}
+int main(void) {
     scanf("%d", &m);
-    unordered_map<int, int> mt;
-    for (int i = 1; i <= N; i ++ ) {
-        int rt = i;
-        while (rt % 10 == 0 && rt != 0) {
-            countTen ++;
-            rt /= 10;
-        }
-        while (rt % 5 == 0 && rt != 0) {
-            countFive ++;
-            rt /= 5;
-        }
-        while (rt % 2 == 0 && rt != 0) {
-            countTwo ++;
-            rt /= 2;
-        }
-
-        mt[i] = min(countTwo, countFive) + countTen;
-    }
-
-    int i = 1;
-    bool tag = false;
-    vector<int> vec;
-    int count = 0;
-    for (; i <= N; i ++ ) {
-        if (mt[i] == m) {
-            count ++;
-            vec.push_back(i);
-            tag = true;
-        }
-
-        if (mt[i] > m && !tag) {
-            cout << 0 << endl;
-            return 0;
+    vector<int> v;
+    for (int i = 1; ; i ++ ) {
+        // 阶乘后缀0的个数
+        int t = f(i, 5);
+        if (t > m) break;
+        if (t == m) {
+            v.push_back(i);
         }
     }
-    cout << count << endl;
-    for (auto& x : vec) {
-        cout << x << " ";
+    if (v.size() == 0) cout << v.size() << endl;
+    else {
+        cout << v.size() << endl;
+        for (auto& x : v) {
+            cout << x << " ";
+        }
     }
     return 0;
 }
