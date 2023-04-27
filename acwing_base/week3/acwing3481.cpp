7ab57f53e0b02c65a5b94a98f809aa95f91135ea
quanhangbo <2409711401@qq.com>
2023-04-27 22:31:44 +0800

    模拟

---
+++ acwing_base/week3/acwing3481.cpp
@@ -0,0 +1,25 @@
+//
+// Created by quanhangbo on 2023/4/27.
+//
+
+#include<iostream>
+using namespace std;
+const int N = 15;
+int sum[N] = {1, 2, 4, 10, 34, 154, 874, 5914, 46234, 409114, 4037914};
+
+int dfs(int num) {
+
+}
+int main(void) {
+    int n;
+    while (cin >> n) {
+        if (n >= 0) {
+            for (int i = 0; i < 10; i ++ ) {
+                for (int j = i + 1; j < 10; j ++ ) {
+
+                }
+            }
+        }
+    }
+    return 0;
+}
